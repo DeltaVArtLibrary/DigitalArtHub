e70a2903f3add6739bdb0d575d98c679f9e8d313
Kozer2 <benhemann@yahoo.com>
2021-04-19 17:24:51 -0500

    Dun got swashbluck werkin Aye Captain Yarrrrr

--- ArtHub/Startup.cs
+++ ArtHub/Startup.cs
@@ -3,10 +3,12 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Swashbuckle.AspNetCore.SwaggerGen;
 
 namespace ArtHub
 {
@@ -16,15 +18,41 @@ namespace ArtHub
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+
+            services.AddControllers();
+            services.AddSwaggerGen(options =>
+            {
+                // Make sure get the "using Statement"
+                options.SwaggerDoc("v1", new OpenApiInfo()
+                {
+                    Title = "Digital ArtHub",
+                    Version = "v1",
+                });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+            public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
+
+
+            app.UseSwagger(options => {
+                options.RouteTemplate = "/api/{documentName}/swagger.json";
+            });
+
+            app.UseSwaggerUI(options =>
+            {
+                options.SwaggerEndpoint("/api/v1/swagger.json", "Digital ArtHub");
+                options.RoutePrefix = "";
+            });
+
+
+
+
 
             app.UseRouting();
 
@@ -32,6 +60,7 @@ namespace ArtHub
             {
                 endpoints.MapGet("/", async context =>
                 {
+                    context.Response.Redirect("/docs");
                     await context.Response.WriteAsync("Hello World!");
                 });
             });
