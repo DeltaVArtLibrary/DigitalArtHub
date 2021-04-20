70e7eb3d3a5cfd42049c251b0a05e28eb5853d0c
jmcia2020 <jeanmmcmahon@gmail.com>
2021-04-20 11:16:13 -0500

    created Art Model and updates to Profile Model

--- ArtHub/Models/Profile.cs
+++ ArtHub/Models/Profile.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,7 +9,11 @@ namespace ArtHub.Models
     public class Profile
     {
         public int ProfileId { get; set; }
+
+        [Required]
         public string DisplayName { get; set; }
+
+        [StringLength(1500)]
         public string Description { get; set; }
     }
 }
