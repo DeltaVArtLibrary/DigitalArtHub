6cbf69f84f6c6131d5bbeee5b486661375624219
Vavyo <vavyo2121@gmail.com>
2021-04-20 10:22:34 -0500

    Profile model with dbset

--- ArtHub/Data/ArtHubDbContext.cs
+++ ArtHub/Data/ArtHubDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using ArtHub.Models;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,5 +14,6 @@ namespace ArtHub.Data
         {
 
         }
+        public DbSet<Profile> Profiles { get; set; }
     }
 }
