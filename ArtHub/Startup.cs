277aaefa2b303943ad50218ca13aa5a5485122b4
Vavyo <vavyo2121@gmail.com>
2021-04-20 09:46:41 -0500

    Folder rename

--- ArtHub/Startup.cs
+++ ArtHub/Startup.cs
@@ -10,7 +10,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Swashbuckle.AspNetCore.SwaggerGen;
 using Microsoft.Extensions.Configuration;
-using ArtHub.data;
+using ArtHub.Data;
 using Microsoft.EntityFrameworkCore;
 
 namespace ArtHub
