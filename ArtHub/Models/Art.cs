d8ca98c7fbd128b5dc6d607adacc7581a97118ad
jmcia2020 <jeanmmcmahon@gmail.com>
2021-04-20 11:30:42 -0500

    added Art model

--- ArtHub/Models/Art.cs
+++ ArtHub/Models/Art.cs
@@ -1,11 +1,33 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace ArtHub
+namespace ArtHub.Models
 {
     public class Art
     {
+        public int ArtId { get; set; }   
+        
+        [Required]
+        public string Content { get; set; }
+
+        [StringLength(1500)]
+        public string Description { get; set; } 
+
+        public string MediaType { get; set; }
+
+        [Timestamp]
+        public DateTime UploadDate { get; set; }
+
+        [Required]
+
+        public int ProfileId { get; set; }
+
+        //Navigation Properties
+
+        public Profile Profile { get; set; }
+
     }
 }
