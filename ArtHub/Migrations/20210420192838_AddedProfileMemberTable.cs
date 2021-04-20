fa5c436e86c0761fb5b14303f88c5a2a93ab41e8
Vavyo <vavyo2121@gmail.com>
2021-04-20 14:31:16 -0500

    Migration

---
+++ ArtHub/Migrations/20210420192838_AddedProfileMemberTable.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ArtHub.Migrations
+{
+    public partial class AddedProfileMemberTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ProfileMembers",
+                columns: table => new
+                {
+                    ProfileId = table.Column<int>(type: "int", nullable: false),
+                    UserId = table.Column<string>(type: "nvarchar(450)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ProfileMembers", x => new { x.ProfileId, x.UserId });
+                    table.ForeignKey(
+                        name: "FK_ProfileMembers_AspNetUsers_UserId",
+                        column: x => x.UserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_ProfileMembers_Profiles_ProfileId",
+                        column: x => x.ProfileId,
+                        principalTable: "Profiles",
+                        principalColumn: "ProfileId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ProfileMembers_UserId",
+                table: "ProfileMembers",
+                column: "UserId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ProfileMembers");
+        }
+    }
+}
