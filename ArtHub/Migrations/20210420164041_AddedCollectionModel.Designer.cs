837c0aa7bff4fee281b4b94f151cdbd86183b1cd
unknown <adard2002@gmail.com>
2021-04-20 11:42:52 -0500

    added Migration for CollectionModel

--- ArtHub/Migrations/20210420164041_AddedCollectionModel.Designer.cs
+++ ArtHub/Migrations/20210420164041_AddedCollectionModel.Designer.cs
@@ -4,14 +4,16 @@ using ArtHub.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace ArtHub.Migrations
 {
     [DbContext(typeof(ArtHubDbContext))]
-    partial class ArtHubDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210420164041_AddedCollectionModel")]
+    partial class AddedCollectionModel
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- ArtHub/Models/Collection.cs
+++ ArtHub/Models/Collection.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +8,17 @@ namespace ArtHub.Models
 {
     public class Collection
     {
+        [Required]
+        public int ProfileId { get; set; }
+
+        public int CollectionId { get; set; }
+
+        [StringLength(1500)]
+        public string Description { get; set; }
+
+        // Navigation Properties
+        public Profile Profile { get; set; }
+
+
     }
 }
