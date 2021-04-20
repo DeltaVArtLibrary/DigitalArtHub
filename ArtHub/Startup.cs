7ba741e4b7c0c934cdd7d407425a78705024b86b
Vavyo <vavyo2121@gmail.com>
2021-04-20 09:35:29 -0500

    Db created run update-database or dotnet ef database update

--- ArtHub/Startup.cs
+++ ArtHub/Startup.cs
@@ -9,6 +9,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Swashbuckle.AspNetCore.SwaggerGen;
+using Microsoft.Extensions.Configuration;
+using ArtHub.data;
+using Microsoft.EntityFrameworkCore;
 
 namespace ArtHub
 {
@@ -16,8 +19,18 @@ namespace ArtHub
     {
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public IConfiguration Configuration { get; }
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<ArtHubDbContext>(options =>
+           {
+               string connectionString = Configuration.GetConnectionString("DefaultConnection");
+               options.UseSqlServer(connectionString);
+           });
 
             services.AddControllers();
             services.AddSwaggerGen(options =>
