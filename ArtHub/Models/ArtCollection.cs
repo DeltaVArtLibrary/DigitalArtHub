4fce1eba9fea4fcd4685aa0dfcea53a93505b04b
jmcia2020 <jeanmmcmahon@gmail.com>
2021-04-20 13:17:28 -0500

    Added Art Collection table

---
+++ ArtHub/Models/ArtCollection.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ArtHub.Models
+{
+    public class ArtCollection
+    {
+        // Navigation Properties
+        public Collection Collection { get; set; }
+        public Art Art { get; set; }
+    }
+}
