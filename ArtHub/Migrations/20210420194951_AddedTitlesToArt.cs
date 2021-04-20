08d062857cbdbb162abcd57bc3290d30ff794955
Vavyo <vavyo2121@gmail.com>
2021-04-20 14:56:21 -0500

    Art table title migration

---
+++ ArtHub/Migrations/20210420194951_AddedTitlesToArt.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ArtHub.Migrations
+{
+    public partial class AddedTitlesToArt : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Title",
+                table: "Art",
+                type: "nvarchar(500)",
+                maxLength: 500,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Title",
+                table: "Art");
+        }
+    }
+}
