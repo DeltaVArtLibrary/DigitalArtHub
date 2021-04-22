8771ee1259f962b13140857f135d1a2529093729
jmcia2020 <jeanmmcmahon@gmail.com>
2021-04-22 10:57:51 -0500

    Art DTO refactored in the Art Repository. Other errors fixed as needed

--- ArtHub/Models/Api/ArtDto.cs
+++ ArtHub/Models/Api/ArtDto.cs
@@ -5,40 +5,37 @@ using System.Threading.Tasks;
 
 namespace ArtHub.Models.Api
 {
+    public class AllArtDto
+    {
+        public int ArtId { get; set; }
+
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public int ProfileId { get; set; }
+
+        public string ProfileDisplayName { get; set; }
+
+    }   
+
     public class ArtDto
     {
         public int ArtId { get; set; }
 
-        public int ProfileId { get; set; }
-
-        public string DisplayName { get; set; }
-
         public string Title { get; set; }
+
+        public string Content { get; set; } 
 
         public string Description { get; set; } 
 
-    }
+        public DateTime UploadDate { get; set; }
 
+        public int ProfileId { get; set; }
 
-    namespace ArtHub.Models.Api
-    {
-        public class ArtPieceDto
-        {
-            public int ArtId { get; set; }
+        public string ProfileDisplayName { get; set; }
 
-            public int ProfileId { get; set; }
-
-            public string DisplayName { get; set; }
-
-            public string Title { get; set; }
-
-            public string Content { get; set; } 
-
-            public string Description { get; set; } 
-
-            public DateTime UploadDate { get; set; }
-
-            //public string MediaType { get; set; }
-        }
+        //public string MediaType { get; set; }
+        
     }
 }
