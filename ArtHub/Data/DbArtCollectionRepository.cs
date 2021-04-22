c53fbbfd41a414c1ffd24f8b722465feef938522
Vavyo <vavyo2121@gmail.com>
2021-04-22 10:00:25 -0500

    Added method to repository

--- ArtHub/Data/DbArtCollectionRepository.cs
+++ ArtHub/Data/DbArtCollectionRepository.cs
@@ -1,4 +1,5 @@
 ﻿using ArtHub.Data.Interfaces;
+using ArtHub.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,6 +14,20 @@ namespace ArtHub.Data
         public DbArtCollectionRepository(ArtHubDbContext context)
         {
             _context = context;
+        }
+
+        public async Task<bool> AddToCollection(ArtCollection artCollection)
+        {
+            if (ArtCollectionExists(artCollection))
+                return false;
+            _context.ArtCollections.Add(artCollection);
+            await _context.SaveChangesAsync();
+            return true;
+        }
+        private bool ArtCollectionExists(ArtCollection artCollection)
+        {
+            return _context.ArtCollections.Any(ac => ac.ArtId == artCollection.ArtId && ac.CollectionId == artCollection.CollectionId);
+
         }
     }
 }
--- ArtHub/Data/Interfaces/IArtCollectionRepository.cs
+++ ArtHub/Data/Interfaces/IArtCollectionRepository.cs
@@ -1,6 +1,10 @@
-﻿namespace ArtHub.Data.Interfaces
+﻿using ArtHub.Models;
+using System.Threading.Tasks;
+
+namespace ArtHub.Data.Interfaces
 {
     public interface IArtCollectionRepository
     {
+        Task<bool> AddToCollection(ArtCollection artCollection);
     }
 }
