f53afe869e1452e36e62c4b310988f6dc3a4948d
Kozer2 <benhemann@yahoo.com>
2021-04-22 18:18:52 -0500

    Changed ProfileCollectionController to need an account

--- ArtHub/Controllers/ProfileCollectionController.cs
+++ ArtHub/Controllers/ProfileCollectionController.cs
@@ -9,9 +9,11 @@ using ArtHub.Data;
 using ArtHub.Models;
 using ArtHub.Data.Interfaces;
 using ArtHub.Models.Api;
+using Microsoft.AspNetCore.Authorization;
 
 namespace ArtHub.Controllers
 {
+    [Authorize]
     [Route("api/Profile/{profileId}/Collection")]
     [ApiController]
     public class ProfileCollectionController : ControllerBase
