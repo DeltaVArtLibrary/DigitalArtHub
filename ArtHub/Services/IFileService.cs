e621863988841b9c1a94e054a2b233c838b2d5cc
Kozer2 <benhemann@yahoo.com>
2021-06-01 13:30:11 -0500

    created the IFileService

---
+++ ArtHub/Services/IFileService.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ArtHub.Services
+{
+    public interface IFileService
+    {
+        Task<string> Create(IFormFile artImage);
+        // need an update
+        Task<string> Update(IFormFile artImage);
+    }
+}
