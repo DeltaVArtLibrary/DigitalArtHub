b18aea12ee2a5dab949dd6a8ed3617918ad9c612
jmcia2020 <jeanmmcmahon@gmail.com>
2021-06-03 13:07:08 -0500

    Added file Model and Controller for Blob handling

---
+++ ArtHub/Controllers/FileController.cs
@@ -0,0 +1,24 @@
+﻿using ArtHub.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ArtHub.Controllers
+{   [Route("api/file")]
+    [ApiController]
+    public class FileController : Controller
+    {
+        [HttpPost]
+        public ActionResult Post([FromForm] FileModel file)
+        {
+            return Ok();
+        }
+        public IActionResult File()
+        {
+            return View();
+        }
+    }
+}
---
+++ ArtHub/Models/FileModel.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ArtHub.Models
+{
+    public class FileModel
+    {
+        public string ArtFile { get; set; }
+        public IFormFile FormFile { get; set; }
+    }
+}
